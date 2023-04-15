bb9f424f10db89269639914a882b8f643fdff9ca
ehall <halle15@email.franklin.edu>
2023-04-15 14:53:06 -0400

    full implementation, documentation added

--- circularqueue.c
+++ circularqueue.c
@@ -2,60 +2,89 @@
 #include <stdlib.h>
 #include "circularqueue.h"
 
-void initQueue(CircularQueue *q){
+/// @brief Initializes the queue to be used, sets proper values
+/// @param q The queue to be initialized
+void initQueue(CircularQueue *q)
+{
     q->front = 0;
     q->rear = 0;
     q->size = 0;
 }
 
-int enqueue(CircularQueue *q, int value){
-    if(isFull(q)){
+/// @brief Enqueues a given value onto a given circular queue
+/// @param q The queue to be changed
+/// @param value The value to be placed in the queue
+/// @return
+int enqueue(CircularQueue *q, int value)
+{
+    if (isFull(q))
+    {
         return -1;
     }
-    
+
     q->data[q->rear] = value;
-    q->rear = (q->rear+1) % BUFFER_SIZE;
+    q->rear = (q->rear + 1) % BUFFER_SIZE;
     q->size++;
     return 0;
 }
 
-int dequeue(CircularQueue *q){
-    if(isEmpty(q)){
+/// @brief Dequeues the next item and returns it.
+/// @param q The queue to remove an item from
+/// @return The item removed from the queue.
+int dequeue(CircularQueue *q)
+{
+    if (isEmpty(q))
+    {
         return -1;
     }
 
     int value = q->data[q->front];
 
     q->front = (q->front + 1) % BUFFER_SIZE;
-    
+
     q->size--;
 
     return value;
 }
 
-int peek(CircularQueue *q){
+/// @brief Returns, but does not remove, from the circular queue.
+/// @param q The queue to peek from
+/// @return The item at the front of the queue that was NOT removed.
+int peek(CircularQueue *q)
+{
     return q->data[q->front];
 }
 
-int isEmpty(CircularQueue *q){
+/// @brief Returns an int whether or not the queue is empty
+/// @param q The queue to check
+/// @return 1 if the queue is empty, 0 if otherwise.
+int isEmpty(CircularQueue *q)
+{
     return q->size == 0;
 }
 
-int isFull(CircularQueue *q){
+/// @brief Returns an int whether or not the queue is full
+/// @param q The queue to check
+/// @return 1 if the queue is full, 0 if otherwise.
+int isFull(CircularQueue *q)
+{
     return q->size == BUFFER_SIZE;
 }
 
-void printAll(CircularQueue *q){
+/// @brief Generates a formatted list to be printed.
+/// @param q The queue to read.
+void printAll(CircularQueue *q)
+{
     printf("{");
     int i, count;
-    for(i = q->front, count = 0; count < q->size; count++){
+    for (i = q->front, count = 0; count < q->size; count++)
+    {
         printf("%d", q->data[i]);
         i = (i + 1) % BUFFER_SIZE;
-        if(count != q->size-1){
+        if (count != q->size - 1)
+        {
             printf(", ");
         }
     }
     printf("}\n");
 }
-
-
--- hall-evann-proj2.c
+++ hall-evann-proj2.c
@@ -3,100 +3,158 @@
 #include "circularqueue.h"
 #include <pthread.h>
 
+//---------How many iterations will the threads run through----------
 #define THREAD_ITERATIONS 1000
-
+//-----------The delay value for the Producer and Consumer threads.-----------------
 #define PRODUCER_DELAY 200000
-#define CONSUMER_DELAY 250000
-
+#define CONSUMER_DELAY 210000
+//--------The upper bound for the randomized sleep value-------------
+#define RANDOMSLEEP_UPPERBOUND 10000
+//---------The upper bound for the value of an item generated.----------
 #define RANDOMITEM_UPPERBOUND 100
-#define RANDOMSLEEP_UPPERBOUND 100000
 
+//------------DECLARATION SECTION
 void *producer(void *param);
 void *consumer(void *param);
 
-CircularQueue q;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-int rnd(int bound){
+// full buffer is to be signaled when the buffer is no longer full, empty is signaled when the buffer is no longer empty.
+pthread_cond_t fullBuffer = PTHREAD_COND_INITIALIZER;
+pthread_cond_t emptyBuffer = PTHREAD_COND_INITIALIZER;
+
+pthread_t thr_1, thr_2;
+pthread_t tid_1, tid_2;
+
+// a barrier is created to force threads to begin executing at a similar time.
+pthread_barrier_t barrier;
+
+CircularQueue q;
+//-------------------------------
+
+/// @brief A small modification of the random number generation function.
+/// @param bound The upper bound of the number to be randomly generated
+/// @return The random number between 0 and the bound
+int rnd(int bound)
+{
     return rand() % bound;
 }
 
-int main(int argc, char *argv[]) {
-
-    printf("started\n");
-    
-    pthread_t thr_1, thr_2;
-
-    pthread_attr_t attr_1, attr_2;
-    pthread_t tid_1, tid_2;
-
-    pthread_attr_init(&attr_1);
-    pthread_attr_init(&attr_2);
-
-    pthread_create(&tid_1, &attr_1, producer, &q);
-    pthread_create(&tid_2, &attr_2, consumer, &q);
-
-    pthread_join(tid_1, NULL);
-    pthread_join(tid_2, NULL);
-
-    return 0;
-    
-}
-
-
-void *producer(void *param) {
-    int item;
-
+void seedRandom()
+{
     struct timespec now;
     clock_gettime(CLOCK_REALTIME, &now);
     srand(now.tv_nsec ^ pthread_self());
+}
 
-    printf("%i\n", pthread_self());
+int main(int argc, char *argv[])
+{
 
+    // sets queue object to starting parameters
+    initQueue(&q);
 
-    while (1) {
+    // creating a barrier object to force the threads to not start until all have begun
+    pthread_barrier_init(&barrier, NULL, 2);
+
+    // creates the threads, passes in the consumer or producer function to be ran, as well as the global queue object.
+    pthread_create(&tid_1, NULL, producer, &q);
+    pthread_create(&tid_2, NULL, consumer, &q);
+
+    // when threads have completed, join them together with relevant params (if not null) and finish execution of program.
+    pthread_join(tid_1, NULL);
+    pthread_join(tid_2, NULL);
+
+    // destroy mutex lock.
+    pthread_mutex_destroy(&mutex);
+    // destroy cond signals
+    pthread_cond_destroy(&emptyBuffer);
+    pthread_cond_destroy(&fullBuffer);
+    // destroy original barrier obj
+    pthread_barrier_destroy(&barrier);
+
+    return 0;
+}
+
+/// @brief The function that represents the producer in the problem. Randomly generates numbers and places them in the Queue.
+/// @param param The global queue being passed in.
+/// @return No output, but fills global queue.
+void *producer(void *param)
+{
+    seedRandom(); // this is called before the barrier to ensure there is a slight difference in recorded time
+
+    pthread_barrier_wait(&barrier); // this is a call to wait for the signal from the barrier to continue
+
+    int item; // declare item
+
+    while (1)
+    {
 
         // Generate a random number
         item = rnd(RANDOMITEM_UPPERBOUND);
 
-        if (enqueue(&q, item)){
-            printf("Failed to put!\n");
-        }
-        else{
-            printf("producer produced %d\n", item);
+        pthread_mutex_lock(&mutex); // this is a call to wait for the signal from the barrier to continue
+
+        // if the buffer is full, the thread waits for the signal from a consumer to ensure that it doesn't overplace.
+        if (isFull(&q))
+        {
+            pthread_cond_wait(&fullBuffer, &mutex);
         }
 
+        if (enqueue(&q, item))
+        {
+            printf("Failed to put!\n"); // THIS SHOULD NOT BE REACHED!
+            pthread_exit(NULL);
+        }
+        else
+        {
+            printf("producer produced %d\n", item); // Success
+        }
+
+        pthread_cond_signal(&emptyBuffer); // signal to consumer threads that it has been added to
+        pthread_mutex_unlock(&mutex);      // allow other threads to access critical resource.
+
         printAll(&q);
-        usleep(PRODUCER_DELAY + rnd(RANDOMSLEEP_UPPERBOUND));
+        usleep(PRODUCER_DELAY + rnd(RANDOMSLEEP_UPPERBOUND)); // randomly calculated delay. Base time is calculated by RND, then added to with the modifier.
     }
 }
 
-void *consumer(void *param) {
-    int item;
+/// @brief The function that represents the consumer in the problem. Randomly eats numbers.
+/// @param param The global queue being passed in.
+/// @return No output, but eats from global queue.
+void *consumer(void *param)
+{
+    seedRandom(); // this is called before the barrier to ensure there is a slight difference in recorded time
 
-    struct timespec now;
-    clock_gettime(CLOCK_REALTIME, &now);
-    srand(now.tv_nsec ^ pthread_self());
+    pthread_barrier_wait(&barrier); // barrier to ensure they begin at similar times.
 
+    int item; // declare item
 
-    printf("%i\n", pthread_self());
+    while (1)
+    {
+        pthread_mutex_lock(&mutex);
 
-
-    while (1) {
+        // ensure that if the queue is empty, wait for a producer thread to signal.
+        if (isEmpty(&q))
+        {
+            pthread_cond_wait(&emptyBuffer, &mutex);
+        }
 
         item = dequeue(&q);
 
-        if (item == -1){
-            printf("Failed to consume!\n");
+        if (item == -1)
+        {
+            printf("Failed to consume!\n"); // SHOULD NOT bE REACHED!
+            pthread_exit(NULL);
         }
-        else{
+        else
+        {
             printf("consumer consumed %d\n", item);
         }
 
+        pthread_cond_signal(&fullBuffer); // signal to producer threads that the last item was eaten.
+        pthread_mutex_unlock(&mutex);
+
         printAll(&q);
-        usleep(CONSUMER_DELAY + rnd(RANDOMSLEEP_UPPERBOUND));
-        }
+        usleep(CONSUMER_DELAY + rnd(RANDOMSLEEP_UPPERBOUND)); // randomly calculated delay. Base time is calculated by RND, then added to with the modifier.
+    }
 }
-
-
-
-
